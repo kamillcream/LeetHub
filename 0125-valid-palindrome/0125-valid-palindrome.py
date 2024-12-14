a4e2b2ecf709e5c52cd26ab5cb9c8f20a10d8a6a
chocolateshell <62238395+Jinyoun9@users.noreply.github.com>
2024-12-14 12:11:57 +0900

    Time: 195 ms (22.06%), Space: 14 MB (23.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+
+        
