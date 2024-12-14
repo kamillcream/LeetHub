2e0ce3ea4132a9ea25deec8a331a110abbcfb83b
chocolateshell <62238395+Jinyoun9@users.noreply.github.com>
2024-12-14 14:05:41 +0900

    Time: 8 ms (23.64%), Space: 12.1 MB (28.29%) - LeetHub

---
+++ 0837-most-common-word/0837-most-common-word.py
@@ -0,0 +1,10 @@
+import collections as collections
+
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+                    .lower().split() if word not in banned]
+        counts = collections.Counter(words)
+
+        return counts.most_common(1)[0][0]
+        
