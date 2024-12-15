3eff2d30c4544888f9692d2cdd1ec67be083bc7b
chocolateshell <62238395+Jinyoun9@users.noreply.github.com>
2024-12-15 12:52:31 +0900

    Time: 0 ms (100%), Space: 12.8 MB (18.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,14 @@ class Solution(object):
     def twoSum(self, nums, target):
         nums_map = {}
 
-        for i, num in enumerate(nums):
-            nums_map[num] = i
+        #for i, num in enumerate(nums):
+        #    nums_map[num] = i
+
+        #for i, num in enumerate(nums):
+        #    if target - num in nums_map and i != nums_map[target - num]:
+        #        return [i, nums_map[target-num]]
 
         for i, num in enumerate(nums):
-            if target - num in nums_map and i != nums_map[target - num]:
-                return [i, nums_map[target-num]]
+            if target - num in nums_map:
+                return [i, nums_map[target - num]]
+            nums_map[num] = i
