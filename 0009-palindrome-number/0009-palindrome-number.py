715c0f4411b2ee9db7d759fc4f6ad3d31556f8b9
chocolateshell <62238395+Jinyoun9@users.noreply.github.com>
2024-12-18 23:57:45 +0900

    Time: 13 ms (41.78%), Space: 12.5 MB (6.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,11 +1,17 @@
-from collections import deque
 class Solution(object):
     def isPalindrome(self, x):
-        # 숫자를 문자열로 변환하여 리스트로 만든다
-        x = str(x)
+        # 음수인 경우 팰린드롬이 될 수 없다.
+        if x < 0:
+            return False
         
-        nums = deque(x)  # deque로 변환
-        while len(nums) > 1:
-            if nums.popleft() != nums.pop():  # 양끝의 값을 비교
-                return False  # 불일치 시 False 반환
-        return True  # 전체 비교가 끝난 후 True 반환
+        # 숫자를 문자열로 변환하여 앞뒤 비교
+        str_x = str(x)
+        left, right = 0, len(str_x) - 1
+        
+        while left < right:
+            if str_x[left] != str_x[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
