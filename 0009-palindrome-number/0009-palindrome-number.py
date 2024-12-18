10e12f2debd040d3759331b9fb01281d1aafcdec
chocolateshell <62238395+Jinyoun9@users.noreply.github.com>
2024-12-18 23:55:27 +0900

    Time: 20 ms (19.26%), Space: 12.4 MB (6.1%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+from collections import deque
+class Solution(object):
+    def isPalindrome(self, x):
+        # 숫자를 문자열로 변환하여 리스트로 만든다
+        x = str(x)
+        
+        nums = deque(x)  # deque로 변환
+        while len(nums) > 1:
+            if nums.popleft() != nums.pop():  # 양끝의 값을 비교
+                return False  # 불일치 시 False 반환
+        return True  # 전체 비교가 끝난 후 True 반환
