5a533aa4fe458593f74f92cb331540e3b5d9ecaa
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-01-06 23:05:53 +0900

    Time: 0 ms (100%), Space: 12.4 MB (25.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,22 @@
 class Solution(object):
     def letterCombinations(self, digits):
         def dfs(index, path):
-            # 끝까지 탐색하면 백트래킹
+            #끝까지 탐색하면 백트래킹
             if len(path) == len(digits):
                 result.append(path)
                 return
             
             for i in range(index, len(digits)):
                 for j in dic[digits[i]]:
-                    dfs(i+1, path+j)
+                    dfs(i+1, path + j)
 
         if not digits:
             return []
 
-        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs",
-        "8": "tuv", "9": "wxyz"}
+        dic = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno",
+            "7":"pqrs","8":"tuv","9":"wxyz"}
 
         result = []
-        dfs(0,"")
+        dfs(0, "")
 
         return result
-
