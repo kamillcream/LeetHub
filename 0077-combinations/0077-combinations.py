d3da83b6fdf73228c00ec9dc862edbac2c18d3f8
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-01-08 19:06:31 +0900

    Time: 263 ms (56.89%), Space: 131.3 MB (31.28%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -6,8 +6,7 @@ class Solution(object):
             if k == 0:
                 res.append(elements[:])
                 return
-
-            #자신 이전의 모든 값을 고정하여 재귀 호출
+            
             for i in range(start, n+1):
                 elements.append(i)
                 dfs(elements, i+1, k-1)
