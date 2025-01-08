54cd6b019c246b6fb511aac5ef45ff37b5d5efb2
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-01-08 19:34:52 +0900

    Time: 11 ms (60.21%), Space: 12.9 MB (29.04%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,4 +1,24 @@
 class Solution(object):
     def permuteUnique(self, nums):
-        return list(set(itertools.permutations(nums)))
+        res = []
+        nums.sort()  # 중복 제거를 위해 정렬
+        visited = [False] * len(nums)  # 방문 여부 추적
         
+        def dfs(path):
+            if len(path) == len(nums):  # 경로가 nums와 길이가 같으면 결과에 추가
+                res.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                # 중복된 숫자는 건너뛰기
+                if visited[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
+                    continue
+                
+                visited[i] = True
+                dfs(path + [nums[i]])  # 다음 경로 탐색
+                visited[i] = False
+        
+        dfs([])
+        return res
