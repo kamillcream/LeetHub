0d1467f4e21859c806e9786088523aba1599434a
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-01-08 19:08:14 +0900

    Time: 26 ms (97.76%), Space: 125.5 MB (95.73%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,3 @@
 class Solution(object):
     def combine(self, n, k):
-        res = []
-
-        def dfs(elements, start, k):
-            if k == 0:
-                res.append(elements[:])
-                return
-            
-            for i in range(start, n+1):
-                elements.append(i)
-                dfs(elements, i+1, k-1)
-                elements.pop()
-        dfs([],1,k)
-        return res
+        return list(itertools.combinations(range(1, n+1), k))
