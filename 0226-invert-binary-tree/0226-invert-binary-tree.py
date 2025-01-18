4a9214f4bdfa0b799e3257310d35fc26ecd93300
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-01-18 23:20:03 +0900

    Time: 0 ms (100%), Space: 12.6 MB (15.79%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def invertTree(self, root):
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return root    
+        
