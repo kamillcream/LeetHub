1359e34f27b6fbe9a63d20cc9d4c360cdf30062e
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-02-19 20:05:50 +0900

    Time: 1 ms (9.96%), Space: 12.5 MB (51.6%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,19 +1,21 @@
-from functools import lru_cache
-
 class Solution(object):
     def numTrees(self, n):
+        memo = {}  
 
-        @lru_cache(None)  
         def count(num):
             if num <= 1:
                 return 1
 
+            if num in memo: 
+                return memo[num]
+
             total = 0
             for i in range(1, num + 1):
-                left_sum = count(i - 1)
-                right_sum = count(num - i)  
-                total += left_sum * right_sum  
-
+                left_sum = count(i - 1)  
+                right_sum = count(num - i) 
+                total += left_sum * right_sum 
+            
+            memo[num] = total 
             return total
 
         return count(n)
