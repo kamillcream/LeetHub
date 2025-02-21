387f682cdedb5005978fa0545df8150818c1a1ed
Jinyoung Park <62238395+kamillcream@users.noreply.github.com>
2025-02-21 21:48:20 +0900

    Time: 7 ms (83.44%), Space: 13.2 MB (12.78%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        if n == 0:
+            return True
+        
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                prev_empty = (i == 0) or (flowerbed[i-1] == 0) # 첫번째 자리거나 왼쪽이 0인지
+                next_empty = (i == len(flowerbed)-1) or (flowerbed[i+1] == 0) # 마지막 자리거나 오른쪽이 0인지
+
+                if prev_empty and next_empty:
+                    flowerbed[i] = 1
+                    n -= 1
+                    if n == 0: # 필요한 만큼 심었으면 종료
+                        return True
+            
+        return n == 0
+            
+        
