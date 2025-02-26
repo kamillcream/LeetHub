2f9b243b746331ac451430bd97635568436aa481
Jinyoung Park <glitt5384@naver.com>
2025-02-26 22:39:51 +0900

    Time: 6 ms (72.26%), Space: 13.5 MB (98.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,3 @@
 class Solution(object):
     def majorityElement(self, nums):
-        if not nums:
-            return None
-        if len(nums) == 1:
-            return nums[0]
-
-        half = len(nums) // 2
-
-        a = self.majorityElement(nums[:half])
-        b = self.majorityElement(nums[half:])
-
-        return [b, a] [nums.count(a) > half]
+        return sorted(nums)[len(nums) // 2]
