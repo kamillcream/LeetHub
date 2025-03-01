5ec2820fb056a37718df3f9258406139849626cb
Jinyoung Park <glitt5384@naver.com>
2025-03-01 23:38:18 +0900

    Time: 4 ms (52.97%), Space: 12.6 MB (43.78%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,24 +1,31 @@
 class Solution(object):
     def diffWaysToCompute(self, input):
-        
-        def compute(left, right, op):
+        memo = {}  # 전역적으로 공유되지 않도록 매 호출마다 새롭게 생성
+
+        def computeWays(expression):
+            if expression in memo:
+                return memo[expression]
+
+            if expression.isdigit():
+                return [int(expression)]
+
             res = []
 
-            for l in left:
-                for r in right:
-                    res.append(eval(str(l) + op + str(r)))
+            for index, value in enumerate(expression):
+                if value in "-+*":  # 연산자일 때만 분할 연산 수행
+                    left = computeWays(expression[:index])
+                    right = computeWays(expression[index + 1:])
+
+                    for l in left:
+                        for r in right:
+                            if value == '+':
+                                res.append(l + r)
+                            elif value == '-':
+                                res.append(l - r)
+                            elif value == '*':
+                                res.append(l * r)
+
+            memo[expression] = res
             return res
-        
-        if input.isdigit():
-            return [int(input)]
 
-        res = []
-
-        for index, value in enumerate(input):
-            if value in "-+*":
-                left = self.diffWaysToCompute(input[:index])
-                right = self.diffWaysToCompute(input[index + 1:])
-
-                res.extend(compute(left, right, value))
-        
-        return res
+        return computeWays(input)
