2589725f9978d9178bfcd0d6b603d2fe3a4e721a
Jinyoung Park <glitt5384@naver.com>
2025-03-02 23:21:14 +0900

    Time: 22 ms (44.8%), Space: 12.3 MB (80.43%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,11 +1,11 @@
 class Solution(object):
     dp = collections.defaultdict(int)
-    def fib(self, n):
-        if n <= 1:
-            return n
-
-        if self.dp[n]:
-            return self.dp[n]
-        self.dp[n] = self.fib(n-1) + self.fib(n-2)
-        return self.dp[n]
         
+    def fib(self, n):
+        self.dp[0] = 0
+        self.dp[1] = 1
+
+        for i in range(2, n+1):
+            self.dp[i] = self.dp[i-1] + self.dp[i-2]
+
+        return self.dp[n]
