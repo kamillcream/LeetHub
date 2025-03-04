5ec5bdc08a06eef2bff814931a3f4fd5a386bf8d
Jinyoung Park <glitt5384@naver.com>
2025-03-04 23:12:14 +0900

    Time: 276 ms (59.08%), Space: 29.1 MB (21.8%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,9 +4,13 @@ class Solution(object):
         deque = collections.deque()
 
         for i in range(len(nums)):
+            # 윈도우 범위 벗어난 값들 제거
+            # ex. deque[0] = 3, i = 4 , k = 1 이면 너비가 1인 윈도우가 4번째에 있으므로 3은 벗어난 거.  
             if deque and deque[0] < i - k + 1:
                 deque.popleft()
 
+            # 새로운 값들보다 작은 값들 제거
+            # 새로 들어온 값보다 작다면 앞으로도 최댓값이 될 수 없음.
             while deque and nums[deque[-1]] < nums[i]:
                 deque.pop()
 
