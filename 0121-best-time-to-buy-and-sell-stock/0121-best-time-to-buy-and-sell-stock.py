c653352e52af4a3fd14bbe8bdf5753d40076d0f1
Jinyoung Park <glitt5384@naver.com>
2025-03-06 20:33:44 +0900

    Time: 135 ms (21.82%), Space: 18.9 MB (90.4%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
-import sys
-
 class Solution(object):
     def maxProfit(self, prices):
         profit = 0
-        min_price = sys.maxsize
-        
+        min_price = float('inf')
+
         for price in prices:
             min_price = min(min_price, price)
             profit = max(profit, price - min_price)
 
         return profit
+        
