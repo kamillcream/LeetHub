6f4544322cb3603edf17c1e8d263d18a81ebb293
Jinyoung Park <glitt5384@naver.com>
2025-03-30 23:57:36 +0900

    Time: 0 ms (100%), Space: 12.6 MB (75.4%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,11 @@
 class Solution(object):
     def generateParenthesis(self, n):
-        result = []
+        dp = [[] for _ in range(n + 1)]
+        dp[0] = [""]
 
-        def backtrack(open_count, close_count, path):
-            # 종료 조건
-            if open_count == n and close_count == n:
-                result.append(path)
-                return
-            
-            # 여는 괄호 추가 가능할 때
-            if open_count < n:
-                backtrack(open_count + 1, close_count, path + "(")
-            
-            # 닫는 괄호 추가 가능할 때
-            if close_count < open_count:
-                backtrack(open_count, close_count + 1, path + ")")
-        
-        backtrack(0, 0, "")
-        return result
+        for i in range(1, n + 1):
+            for j in range(i):
+                for left in dp[j]:
+                    for right in dp[i - 1 - j]:
+                        dp[i].append("(" + left + ")" + right)
+        return dp[n]
