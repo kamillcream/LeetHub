bfa143ac938315334b06ff3971d6bd1f4ff19326
Jinyoung Park <glitt5384@naver.com>
2025-09-18 02:46:05 +0900

    Time: 16 ms (95.24%), Space: 12.5 MB (75.83%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,22 +2,21 @@ class Solution(object):
     def isInterleave(self, s1, s2, s3):
         n, m, l = len(s1), len(s2), len(s3)
 
-        if n + m != l:
+        if n+m != l:
             return False
 
-        dp = [False] * (m + 1)
+        dp = [False] * (n + 1)
         dp[0] = True
 
-        for j in range(1, m + 1):
-            dp[j] = dp[j-1] and s2[j-1] == s3[j-1]
+        for i in range(1, n+1):
+            dp[i] = dp[i-1] and s1[i-1] == s3[i-1]
 
-        for i in range(1, n + 1):
-            dp[0] = dp[0] and s1[i-1] == s3[i-1]
-            for j in range(1, m + 1):
-                dp[j] = (dp[j] and s1[i-1] == s3[i+j-1]) \
-                      or (dp[j-1] and s2[j-1] == s3[i+j-1])
+        for i in range(1, m+1):
+            dp[0] = dp[0] and s2[i-1] == s3[i-1]
+            for j in range(1, n+1):
+                dp[j] = (dp[j] and s2[i-1] == s3[i+j-1]) \
+                    or (dp[j-1] and s1[j-1] == s3[i+j-1])
 
-        return dp[m]
+        return dp[n]
 
 
-        
