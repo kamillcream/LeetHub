cfdcc5570bfcd3960539d4cfe0539e2761ce012d
Jinyoung Park <glitt5384@naver.com>
2025-11-29 19:03:18 +0900

    Time: 49 ms (6.75%), Space: 12.6 MB (40.68%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,12 +1,4 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        answer = []
-        nums_first_half = nums[:n]
-        nums_second_half = nums[n:]
-
-        for i in range(n):
-            answer.append(nums_first_half[i])
-            answer.append(nums_second_half[i])
-
-        return answer
+        return [x for pair in zip(nums[:n], nums[n:]) for x in pair]
         
