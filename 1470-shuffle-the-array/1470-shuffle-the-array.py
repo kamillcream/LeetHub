3d5cdd8510455d297951d244ca96b0fbd489b5d2
Jinyoung Park <glitt5384@naver.com>
2025-11-29 19:00:12 +0900

    Time: 39 ms (61.26%), Space: 12.6 MB (69%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        answer = []
+        nums_first_half = nums[:n]
+        nums_second_half = nums[n:]
+
+        for i in range(n):
+            answer.append(nums_first_half[i])
+            answer.append(nums_second_half[i])
+
+        return answer
+        
