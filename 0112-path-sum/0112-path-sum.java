15d79d1f31422c0b6c2c12781522589c22dc273a
Jinyoung Park <glitt5384@naver.com>
2026-03-21 23:52:03 +0900

    Time: 0 ms (100%), Space: 45.2 MB (24.7%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,18 +14,12 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return dfs(root, 0, targetSum);
-    }
-
-    private boolean dfs(TreeNode node, int curSum, int targetSum) {
-        if (node == null) return false;
-        
-        curSum += node.val;
-
-        if(node.left == null && node.right == null) {
-            return curSum == targetSum;
-        }        
-        return dfs(node.left, curSum, targetSum) || dfs(node.right, curSum, targetSum);
+    public boolean hasPathSum (TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        int remain = targetSum - root.val;
+        return hasPathSum(root.left, remain) || hasPathSum(root.right, remain);
     }
 }
